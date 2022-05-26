37287bdeff000d51c4d9544a2d350cfb4454a7dc
SantiagoGM19 <santigm1905@gmail.com>
2022-05-26 15:06:41 -0500

    kata 6 solved

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -3,7 +3,10 @@ package katas;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -14,6 +17,18 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        Integer largestBoxArt = movies.stream()
+                .map(movie -> movie.getBoxarts())
+                .map(boxArts -> boxArts.stream()
+                        .map(boxArt -> boxArt.getHeight() * boxArt.getWidth())
+                        .reduce(0,(largest, size) -> size > largest? size: largest)).max(Integer::compare).get();
+
+        return movies.stream()
+                .map(movie -> movie.getBoxarts())
+                .map(boxArts -> boxArts.stream()
+                        .filter(boxArt -> (boxArt.getWidth() * boxArt.getHeight() == largestBoxArt))
+                        .map(boxArt -> boxArt.getUrl()).collect(Collectors.toList()))
+                .filter(url -> !url.isEmpty())
+                .collect(Collectors.toList()).get(0).get(0);
     }
 }
