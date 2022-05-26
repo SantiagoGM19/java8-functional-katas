f27d982bb65d834e4ece0305b44fc43589455642
SantiagoGM19 <santigm1905@gmail.com>
2022-05-26 12:14:06 -0500

    kata 4 solved

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -6,8 +6,11 @@ import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -17,7 +20,17 @@ import java.util.Map;
 public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
-
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return movieLists
+                .stream()
+                .flatMap(movieList -> movieList.
+                        getVideos()
+                        .stream()
+                        .map(video -> ImmutableMap.of(video.getId(), ImmutableList.of(video.getTitle(), video.getBoxarts()
+                                .stream()
+                                .filter(boxArt -> boxArt.getWidth() == 150 && boxArt.getHeight() == 200)
+                                .map(boxArt -> ImmutableList.of(boxArt.getWidth(), boxArt.getHeight(), boxArt.getUrl()))
+                                .collect(Collectors.toCollection(ArrayList::new))))))
+                        .collect(Collectors.toCollection(ArrayList::new));
+        /*return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));*/
     }
 }
