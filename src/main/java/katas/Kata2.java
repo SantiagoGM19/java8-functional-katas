33a4ec5f2e09b8d849c7c786876b12b11577571a
SantiagoGM19 <santigm1905@gmail.com>
2022-05-25 23:26:41 -0500

    Kata 2 solved

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -5,8 +5,10 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -16,7 +18,10 @@ import java.util.Map;
 public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(1, 2, 3);
+        return  movies.stream()
+                .filter(movie -> movie.getRating() == 5.0)
+                .map(movie -> movie.getId())
+                .collect(Collectors.toCollection(ArrayList::new));
+        /*return ImmutableList.of(1, 2, 3);*/
     }
 }
